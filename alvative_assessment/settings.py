bd4ae4568b12ad9e39742b1e3d63d909a76f9edb
KimBitrus26 <bitruschoji4real@gmail.com>
2023-03-09 07:50:46 +0100

    update

--- alvative_assessment/settings.py
+++ alvative_assessment/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://kimtemp.herokuapp.com', 'http://kimbitrus.pythonanywhere.com/', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'kimtemp.herokuapp.com', 'kimbitrus.pythonanywhere.com', 'localhost']
 
 
 # Application definition
